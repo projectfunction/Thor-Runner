40c100710bea0b5bffe0dbaba3b774a3f77befdd
Daryl <darylcecile@gmail.com>
2022-04-30 19:25:51 +0100

    added logging

--- index.ts
+++ index.ts
@@ -1,10 +1,13 @@
 import { exec, spawn } from "child_process";
-import {convertToKebabCase} from "./utils";
+import {nanoid} from "nanoid";
+import * as fs from "fs/promises";
+import {join} from "path";
 
 type ThorRunnerRunOptions = {
 	targetLocation: string,
-	permissions?: Record<string, string>,
-	params?: string[],
+	repoUrl: string,
+	permissions?: Array<string>,
+	params?: Array<string>,
 	onOutput?: (data:string, isStdErr?:boolean) => void,
 	onExit?: (exitCode:number) => void
 }
@@ -20,24 +23,70 @@ export default class ThorRunner {
 		})
 	}
 
-	static run(options:ThorRunnerRunOptions){
-		let permissions = Object.entries(options.permissions ?? {})
-			.map(entry => `--${convertToKebabCase(entry[0])}=${entry[1]}`)
-		let child = spawn(`deno run ${permissions} ${options.targetLocation} ${(options.params ?? []).join(' ')}`);
+	static async getDenoPath(){
+		return new Promise<string>((resolve, reject) => {
+			exec('which deno', (error, stdout, stderr) => {
+				if (!!error) return reject(error);
+				return resolve(stdout.trim());
+			});
+		});
+	}
+
+	static async exec(command:string, cwd:string){
+		return new Promise<string>((resolve, reject) => {
+			exec(command, {cwd}, (error, stdout, stderr) => {
+				if (!!error) return reject(error);
+				resolve(stdout);
+			});
+		});
+	}
+
+	static async run(options:ThorRunnerRunOptions){
+		const instanceId = nanoid(12);
+		const dirName = join(process.cwd(), instanceId);
+		await fs.mkdir(dirName);
+
+		let isRunning = true;
+
+		options.onOutput?.(`Cloning ${options.repoUrl}`);
+		await ThorRunner.exec(`git clone ${options.repoUrl}`, dirName).catch(e => console.error(e));
+		options.onOutput?.('Cloned!');
+
+		let permissions = [...options.permissions, `--allow-read=${dirName}`, '--allow-env=THOR_CWD'];
+
+		options.onOutput?.(`Starting instance ${instanceId}...`);
+
+		let child = spawn(
+			await ThorRunner.getDenoPath().catch(()=>'deno'),
+			['run', ...permissions, options.targetLocation, ...(options.params ?? [])],
+			{ cwd: dirName, env: { ...process.env, 'THOR_CWD':dirName } }
+		);
 
 		child.stdout.on('data', data => {
-			options.onOutput?.(data, false);
+			let lines = data.toString().split(/\r?\n/).filter(l => !!l && l.trim?.().length !== 0);
+			lines.forEach(line => options.onOutput?.(line, false));
 		});
 
 		child.stderr.on('data', data => {
-			options.onOutput?.(data, true);
+			let lines = data.toString().split(/\r?\n/).filter(l => !!l && l.trim?.().length !== 0);
+			lines.forEach(line => options.onOutput?.(line, true));
 		});
 
 		child.on('close', (code) => {
-			options.onExit?.(code);
+			isRunning = false;
+			options.onOutput?.(`Cleaning up instance ${instanceId}...`);
+			fs.rm(dirName, { recursive: true }).finally(()=>{
+				options.onOutput?.(`Done.`);
+				options.onExit?.(code ?? 0);
+			});
 		});
 
-		return { kill: (code?:number) => child.kill(code) }
+		return {
+			kill: (code?:number) => child.kill(code),
+			path: dirName,
+			instanceId: instanceId,
+			checkIsRunning: ()=>isRunning
+		}
 	}
 
 }
--- utils.ts
+++
@@ -1,5 +0,0 @@
-
-
-export function convertToKebabCase(input:string){
-	return input.replace(/[a-z][A-Z]/g, (a,b,c)=> a[0] + '-' + a.substring(1).toLowerCase() );
-}
